a8a1c990426fc75b27aec877665052bef438e64a
Jae Kang <jae.kang@wsp.com>
2023-11-23 09:18:56 -0700

    fix navbar height

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -5,8 +5,8 @@ function Navbar() {
     <>
       <div
         id="navBar"
-        className="flex items-center
-                    w-screen h-12 p-3
+        className="navBar flex items-center
+                    w-screen p-3
                     top-0  left-0 sticky z-40 
                     bg-white border-b-2 border-black 
                     font-extrabold text-lg underline"
